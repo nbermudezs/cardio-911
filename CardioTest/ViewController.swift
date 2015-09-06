6e056bf8b38746d1447a1c58f40a7e519840cf59
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-09-05 20:35:46 -0600

    Initial Commit

---
+++ CardioTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardioTest
+//
+//  Created by Nestor Bermudez Sarmiento on 9/5/15.
+//  Copyright © 2015 Nestor Bermudez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

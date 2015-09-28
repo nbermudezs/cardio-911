470fbaf3c090131a04d35e248a25b97a40c26f0d
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-09-27 21:52:48 -0600

    Override call making method for tropo

--- CardioTestWatchApp Extension/TropoGateway.swift
+++ CardioTestWatchApp Extension/TropoGateway.swift
@@ -14,9 +14,11 @@ class TropoGateway: Gateway {
     let host = "https://api.tropo.com"
     let apiVersion = "1.0"
     let restApiManager = RestApiManager(responseType: .XML)
+
+    var voiceAuthUrl: String = ""
     
     override init() {
-
+        voiceAuthUrl = String(format: "%@/%@/sessions?action=create&token=%@", host, apiVersion, voiceApiKey)
     }
 
     override func sendSmsNotification(from: String, to: String, body: String) {
@@ -25,13 +27,53 @@ class TropoGateway: Gateway {
     override func sendTtsSms(from: String, to: String, body: String) {
 
     }
-    
-    private func notifyUserInTroubles(number: String, name: String) {
-        let url = String(format: "%@/%@/sessions?action=create&token=%@&number=%@", host, apiVersion, voiceApiKey, number)
-        restApiManager.makeHTTPGetRequest(url) { (data, error) -> Void in
+
+    override func sendVoiceCall(from: String, to: String, data: [String : String]) {
+        var initial = ["from": from, "to": to]
+        initial.merge(data)
+        let fromData = String.queryStringFromParameters(initial)
+        let url = String(format: "%@&%@", voiceAuthUrl, fromData!)
+
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
+        request.HTTPMethod = "GET"
+
+        self.makeRequest(request) { dict, error in
             if error == nil {
                 self.delegate?.callDidSucceed()
             }
         }
+    }
+
+    private func makeRequest(request: NSMutableURLRequest, onCompletion: ServiceResponse) {
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        let session = NSURLSession(configuration: config)
+
+        let task = session.dataTaskWithRequest(request, completionHandler: {data, response, error -> Void in
+            if let headers = response as? NSHTTPURLResponse {
+                let statusCode = headers.statusCode
+                let parsed = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print(parsed)
+                if statusCode == 200 {
+                    if let responseDictionary = try? self.handleXmlResponse(data) {
+                        onCompletion(responseDictionary, nil)
+                    } else {
+                        onCompletion(nil, nil)
+                    }
+                } else {
+                    onCompletion(nil, NSError(domain: "MakeRequestResponseError", code: statusCode, userInfo: nil))
+                }
+            } else {
+                onCompletion(nil, NSError(domain: "EmptyResponse", code: 500, userInfo: nil))
+            }
+        })
+        task.resume()
+    }
+
+    private func handleXmlResponse(data: NSData?) throws -> [String: AnyObject]? {
+        let jsonObject = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers)
+        if let dict = jsonObject as? [String: AnyObject] {
+            return dict
+        }
+        return nil
     }
 }

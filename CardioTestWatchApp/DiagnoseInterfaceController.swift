730f7010c5eda77712d3cd35fd97dd896f28b60b
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-09-27 21:56:37 -0600

    Add auto-call contact, restart logic and refactoring

--- CardioTestWatchApp/DiagnoseInterfaceController.swift
+++ CardioTestWatchApp/DiagnoseInterfaceController.swift
@@ -23,8 +23,10 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
     // MARK: Properties
     let healthStore : HKHealthStore = HKHealthStore()
     let gateway: Gateway
+    let alertWaitTime: Int = 30 // seconds
+    var timer: NSTimer? = nil
     
-    var sessionManager : WorkoutSessionManager
+    var sessionManager : WorkoutSessionManager? = nil
     var useFakeAfHeartRate = false
     var beatDuration = 0.0
     
@@ -33,23 +35,39 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
             return self.beatDuration / 2.0
         }
     }
+
+    // MARK: Flags
     var notAlertedYet = true
     var playHapticFeedback = true
+    var alertResponded = false
     
     // MARK: Overrides
     
     override init() {
-        let context = WorkoutSessionContext(healthStore: healthStore, activityType: .Other, locationType: .Unknown)
-        sessionManager = WorkoutSessionManager(context: context)
-        gateway = RestCommGateway()
+        gateway = TropoGateway()
 
         super.init()
-        
-        sessionManager.delegate = self
+
+        self.initWorkoutSessionManager()
         gateway.delegate = self
     }
     
     override func willActivate() {
+        self.tryStartAnalysis()
+        self.startAnimation()
+    }
+    
+    override func didDeactivate() {
+        
+    }
+
+    private func initWorkoutSessionManager() {
+        let context = WorkoutSessionContext(healthStore: healthStore, activityType: .Other, locationType: .Unknown)
+        self.sessionManager = WorkoutSessionManager(context: context)
+        self.sessionManager?.delegate = self
+    }
+
+    private func tryStartAnalysis() {
         let typesToShare = Set([HKObjectType.workoutType()])
         let typesToRead = Set([
             HKObjectType.quantityTypeForIdentifier(HKQuantityTypeIdentifierHeartRate)!
@@ -61,15 +79,14 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
                 self.healthAccessDenied()
             }
         }
-        NSTimer.scheduledTimerWithTimeInterval(1,
+    }
+
+    private func startAnimation() {
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(1,
             target: self,
             selector: Selector("animateHeart"),
             userInfo: nil,
             repeats: true)
-    }
-    
-    override func didDeactivate() {
-        
     }
     
     // MARK: Animation
@@ -77,16 +94,16 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
     func animateHeart() {
         if self.animationSpeed > 0 {
             self.animateWithDuration(self.animationSpeed) { () -> Void in
-                self.heartIcon.setWidth(60)
-                self.heartIcon.setHeight(60)
+                self.heartIcon.setWidth(70)
+                self.heartIcon.setHeight(90)
             }
             let when = dispatch_time(DISPATCH_TIME_NOW, Int64(self.animationSpeed * double_t(NSEC_PER_SEC)))
             let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
             dispatch_after(when, queue) { () -> Void in
                 dispatch_async(dispatch_get_main_queue(), {
                     self.animateWithDuration(self.animationSpeed, animations: { () -> Void in
-                        self.heartIcon.setWidth(50)
-                        self.heartIcon.setHeight(50)
+                        self.heartIcon.setWidth(60)
+                        self.heartIcon.setHeight(80)
                     })
                 })
             }
@@ -95,7 +112,7 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
 
     // MARK: Authorization handling
     private func healthAccessAuthorized() {
-        self.sessionManager.startWorkout()
+        self.sessionManager?.startWorkout()
     }
 
     private func healthAccessDenied() {
@@ -105,14 +122,19 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
     // MARK: View Actions
     
     @IBAction func finishDiagnostic() {
-        self.sessionManager.stopWorkoutAndSave()
+        self.sessionManager?.stopWorkoutAndSave()
     }
     
     @IBAction func toggleFakeAf(value: Bool) {
         self.useFakeAfHeartRate = value
-        sessionManager.useFakeAfHeartRate = value
+        sessionManager?.useFakeAfHeartRate = value
     }
     
+    @IBAction func restartAnalysis() {
+        self.finishDiagnostic()
+        self.initWorkoutSessionManager()
+        self.tryStartAnalysis()
+    }
     // MARK: Emergency handling
     
     private func alertUser() {
@@ -124,18 +146,21 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
     }
     
     private func showAlertControllerWithStyle(style: WKAlertControllerStyle!) {
-        
+        self.alertResponded = false
+
         let cancelAction = WKAlertAction(
             title: "No",
             style: WKAlertActionStyle.Cancel) { () -> Void in
                 // TODO: Add action to re-enable it, maybe a "I'm OK" button the user can click
                 self.playHapticFeedback = false
                 print("Haptic Feedback disabled")
+                self.unscheduleAutoNotify()
         }
         
         let notifyAction = WKAlertAction(
             title: "Yes",
             style: WKAlertActionStyle.Destructive) { () -> Void in
+                self.unscheduleAutoNotify()
                 self.callContact()
         }
         
@@ -144,11 +169,32 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
             message: "Your heart rate went crazy for a moment. Want to notify someone?",
             preferredStyle: style,
             actions: [notifyAction, cancelAction])
+
+        self.scheduleAutoNotify()
+    }
+
+    private func scheduleAutoNotify() {
+        let when = dispatch_time(DISPATCH_TIME_NOW, Int64(Double(self.alertWaitTime) * double_t(NSEC_PER_SEC)))
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        dispatch_after(when, queue) { () -> Void in
+            dispatch_async(dispatch_get_main_queue(), {
+                if !self.alertResponded {
+                    self.dismissController()
+                    self.callContact()
+                }
+            })
+        }
+
+    }
+
+    private func unscheduleAutoNotify() {
+        self.alertResponded = true
     }
     
     private func callContact() {
-        let name = NSFullUserName()
-        gateway.sendSmsNotification("", to: "+3122398676", body: String(format: "Hey this is %@", name))
+        let name = "Nestor Bermudez" // NSFullUserName()
+        let location = self.getCurrentLocation() // get location using location API
+        gateway.sendVoiceCall("+13122011010", to: "+13122398676", data: ["name": name, "location": location])
     }
 
     // MARK: Implement WorkoutSessionManagerDelegate
@@ -159,6 +205,7 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
 
     func workoutSessionManager(workoutSessionManager: WorkoutSessionManager, didStopWorkoutWithDate stopDate: Bool) {
         print("workout ended")
+        self.beatDuration = 0.0
     }
 
     func workoutSessionManager(workoutSessionManager: WorkoutSessionManager, didUpdateHrvAnalysis result: AtrialFibrilationDiagnosis) {
@@ -199,5 +246,11 @@ class DiagnoseInterfaceController: WKInterfaceController, WorkoutSessionManagerD
             message: "Your contact has been notified",
             preferredStyle: WKAlertControllerStyle.Alert,
             actions: [okAction])
+    }
+
+    // MARK: Geo location
+
+    private func getCurrentLocation() -> String {
+        return "Burger King, Central Park"
     }
 }

320cfea0fcb7831bca73face93d701735bc1b34a
Nestor Bermudez <nestor.bermudez@agilityfeat.com>
2015-09-27 21:42:06 -0600

    Add extensions to string and dict

---
+++ CardioTestWatchApp Extension/Extensions.swift
@@ -0,0 +1,48 @@
+//
+//  Extensions.swift
+//  CardioTest
+//
+//  Created by Nestor Bermudez Sarmiento on 9/27/15.
+//  Copyright © 2015 Nestor Bermudez. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    func URLEncodedString() -> String? {
+        let customAllowedSet =  NSCharacterSet.URLQueryAllowedCharacterSet()
+        let escapedString = self.stringByAddingPercentEncodingWithAllowedCharacters(customAllowedSet)
+        return escapedString
+    }
+    static func queryStringFromParameters(parameters: Dictionary<String,String>) -> String? {
+        if (parameters.count == 0)
+        {
+            return nil
+        }
+        var queryString : String? = nil
+        for (key, value) in parameters {
+            if let encodedKey = key.URLEncodedString() {
+                if let encodedValue = value.URLEncodedString() {
+                    if queryString == nil
+                    {
+                        queryString = ""
+                    }
+                    else
+                    {
+                        queryString! += "&"
+                    }
+                    queryString! += encodedKey + "=" + encodedValue
+                }
+            }
+        }
+        return queryString
+    }
+}
+
+extension Dictionary {
+    mutating func merge<K, V>(dict: [K: V]){
+        for (k, v) in dict {
+            self.updateValue(v as! Value, forKey: k as! Key)
+        }
+    }
+}
